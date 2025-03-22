4106e3cf033f0482448f4407b0ed63e1dab7caec
Zsolt Moravcsik <zsolt.moravcsik@knowledgeseed.ch>
2025-03-22 18:13:41 +0100

    dev

---
+++ tests/test_unit.py
@@ -0,0 +1,8 @@
+
+def tm1_connection():
+    """Creates a TM1 connection before tests and closes it after all tests."""
+    config = configparser.ConfigParser()
+    config.read(Path(__file__).parent.joinpath('config.ini'))
+
+    tm1 = TM1Service(**config['testbench'])
+    return tm1
